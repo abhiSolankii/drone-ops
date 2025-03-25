5add659e6d8e3174fec09eb4e6ee0f7dc2fa05ee
Abhishek Solanki <abhisheksolanki01010@gmail.com>
2025-03-25 22:15:37 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -36,7 +36,14 @@ const io = new Server(httpServer, {
 });
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(
   rateLimit({
